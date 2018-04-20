fdf62a284e3bba1d7563a3b0450e916f98689f03
Efe Can Cevher <37309169+efe-cevher@users.noreply.github.com>
2018-04-21 00:33:06 +0300

    1

--- src/main/PriorityQueue.java
+++ src/main/PriorityQueue.java
@@ -1,5 +1,75 @@
 package main;
 
-public class PriorityQueue {
+public class PriorityQueue<T extends Comparable<? super T>> implements PriorityQueueInterface<T> {
+
+	private Node<T> head;
+    private int size;
+	
+	public void add(T newEntry) {
+		
+		Node<T> node = head;
+		head = add(node, newEntry);
+	}
+	
+	private Node<T> add(Node<T> node, T data) {
+		
+		Node<T> newNode =  createNewNode(data);
+		if(node == null) 
+		{
+			return newNode;
+		}
+		
+		
+	}
+	
+	private Node<T> createNewNode(T data) {
+		size++;
+		return new Node<T>(data);
+		}
+		
+
+	@Override
+	public T remove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private static class Node<T>
+	{
+		private Node<T> next;
+		private final T data;
+		private final T priority;
+		
+		public Node(T data, T priority)
+		{
+		this.data = data;
+		this.priority = priority;
+		}
+	}
 
 }
+
