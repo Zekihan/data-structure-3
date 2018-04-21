f4a170d6944d2119a10e1c449f7161d7d3ebc370
Efe Can Cevher <37309169+efe-cevher@users.noreply.github.com>
2018-04-21 13:09:15 +0300

    removed test

--- src/main/Test.java
+++ src/main/Test.java
@@ -3,14 +3,7 @@ package main;
 public class Test {
 
 	public static void main(String[] args) {
-		PriorityQueueInterface<String> shoppinglist = new LinkedPQ<String>();
 		
-		System.out.println(shoppinglist.getSize());
-		shoppinglist.add("hi");
-		System.out.println(shoppinglist.getSize());
-		System.out.println(shoppinglist.remove());
-		shoppinglist.clear();
-		System.out.println(shoppinglist.remove());
 	}
 
 }
